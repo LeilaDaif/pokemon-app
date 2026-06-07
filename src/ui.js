a18bf65f4ef2507696ec1b37f3eaf0ef7168fe12
LeilaDaif <Leiladaif@gmail.com>
2026-06-07 13:07:11 +0200

    afbeeldingen toegevoegd

--- src/ui.js
+++ src/ui.js
@@ -1,8 +1,25 @@
-export function displayPokemon(pokemonList){
-    const grid = document.getElementById("pokemon-grid");
-    pokemonList.forEach(pokemon => {
-       const card = document.createElement('div')
-       card.textContent = pokemon.name
-       grid.appendChild(card)
-    });
+import { fetchPokemonData } from "./api.js";
+
+//kaartjes aanmaken
+// hier heb ik geen async nodig want geen await nodig
+export function createCard(pokemon, detail) {
+    const card = document.createElement('div');
+    card.textContent = pokemon.name;
+    const img = document.createElement('img');
+    img.src = detail.sprites.front_default
+    card.appendChild(img)
+    return card
+    
 }
+
+// Kaartjes tonen
+export async function displayPokemon(pokemonList){
+    const grid = document.getElementById("pokemon-grid");
+    for (const pokemon of pokemonList){
+         const detail = await fetchPokemonData(pokemon.url)
+         const card = createCard(pokemon, detail)
+        grid.appendChild(card)
+    }
+}
+
+
