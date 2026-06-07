195d29b5e4b615da1bdc08cf69ec179598525df2
LeilaDaif <Leiladaif@gmail.com>
2026-06-07 19:30:06 +0200

    favortieten toegevoegd

--- src/ui.js
+++ src/ui.js
@@ -1,24 +1,32 @@
+import { getFavorite, saveFavorite, deleteFavorite } from "./storage.js";
 //kaartjes aanmaken
 // hier heb ik geen async nodig want geen await nodig
 export function createCard(pokemon) {
-    const card = document.createElement('div');
-    card.textContent = pokemon.name;
-    const img = document.createElement('img');
-    img.src = pokemon.sprites.front_default
-    card.appendChild(img)
-    return card
-    
+  const card = document.createElement("div");
+  card.textContent = pokemon.name;
+  const img = document.createElement("img");
+  img.src = pokemon.sprites.front_default;
+  card.appendChild(img);
+  const like = document.createElement("button");
+  like.textContent = '\u{1F90D}';
+  card.appendChild(like);
+  
+  let isFavoriet = false;
+  like.addEventListener('click', () => {
+  isFavoriet = !isFavoriet
+  like.textContent= isFavoriet ? '\u2764\uFE0F' : '\u{1F90D}'
+  isFavoriet ? saveFavorite(pokemon) : deleteFavorite(pokemon)
+});
+
+  return card;
 }
 
 // Kaartjes tonen
-export function displayPokemon(pokemonList){
-    const grid = document.getElementById("pokemon-grid");
-    grid.innerHTML = ''
-    pokemonList.forEach(pokemon => {
-          const card = createCard(pokemon)
-          grid.appendChild(card)
-        })
-    };
-        
-
-
+export function displayPokemon(pokemonList) {
+  const grid = document.getElementById("pokemon-grid");
+  grid.innerHTML = "";
+  pokemonList.forEach((pokemon) => {
+    const card = createCard(pokemon);
+    grid.appendChild(card);
+  });
+}
