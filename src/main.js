1854dcf92a7106520ce1a73380745ad1adf9e7b1
LeilaDaif <Leiladaif@gmail.com>
2026-06-07 15:41:01 +0200

    zoek- en filterfunctie toegevoegd

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import { fetchPokemon } from './api.js'
+import { fetchPokemon, fetchPokemonData } from './api.js'
 import { displayPokemon } from './ui.js'
 
 let allePokemon =[];
 async function app(){
   const data = await fetchPokemon();
-  allePokemon = data.results
+  allePokemon = await Promise.all(data.results.map(pokemon => fetchPokemonData(pokemon.url)))
   displayPokemon(allePokemon)
+  types()
 }
-
 app()
 
 
@@ -15,8 +15,29 @@ app()
 const search = document.getElementById('search');
 search.addEventListener('input', () => {
   const gefilterd = allePokemon.filter(pokemon => 
-    pokemon.name.includes(search.value.toLowerCase()) 
+    pokemon.name.includes(search.value.toLowerCase())
 )
 displayPokemon(gefilterd)
 })
 
+async function types(){
+  const filter = document.getElementById('filter-type')
+  const toegevoegdeType = []
+  for(let pokemon of allePokemon){
+    const type = pokemon.types[0].type.name; 
+    if(!toegevoegdeType.includes(type)){
+      toegevoegdeType.push(type)
+    const option = document.createElement('option');
+    option.value = type
+    option.textContent = type
+    filter.appendChild(option)
+    }
+  }
+    filter.addEventListener('change', () => {
+    const gekozenType = filter.value
+    const gefilterd = allePokemon.filter(pokemon => 
+       pokemon.types[0].type.name === gekozenType)
+       displayPokemon(gefilterd)
+  })
+
+}
--- src/ui.js
+++ src/ui.js
@@ -1,26 +1,24 @@
-import { fetchPokemonData } from "./api.js";
-
 //kaartjes aanmaken
 // hier heb ik geen async nodig want geen await nodig
-export function createCard(pokemon, detail) {
+export function createCard(pokemon) {
     const card = document.createElement('div');
     card.textContent = pokemon.name;
     const img = document.createElement('img');
-    img.src = detail.sprites.front_default
+    img.src = pokemon.sprites.front_default
     card.appendChild(img)
     return card
     
 }
 
 // Kaartjes tonen
-export async function displayPokemon(pokemonList){
+export function displayPokemon(pokemonList){
     const grid = document.getElementById("pokemon-grid");
     grid.innerHTML = ''
-    for (const pokemon of pokemonList){
-         const detail = await fetchPokemonData(pokemon.url)
-         const card = createCard(pokemon, detail)
-        grid.appendChild(card)
-    }
-}
+    pokemonList.forEach(pokemon => {
+          const card = createCard(pokemon)
+          grid.appendChild(card)
+        })
+    };
+        
 
 
